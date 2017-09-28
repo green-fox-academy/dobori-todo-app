61fa69ca71372b78fbc65cdad8cd6b7be7083c96
Borbala Dobo <dobori@gmail.com>
2017-09-28 16:36:22 +0200

    fix(todo_list): listing and new item bugs are fixed

--- todo.py
+++ todo.py
@@ -9,34 +9,41 @@ class Todo_controller(object):
 
     def __init__(self):
         if len(sys.argv) == 2 and sys.argv[1] == "-l":
-            print("listazz")
+            return self.todo_list_read()
         elif len(sys.argv) == 2 and sys.argv[1] == "-a":
-            print("adj hozza ujat")
+            return self.add_todo()
         elif len(sys.argv) == 2 and sys.argv[1] == "-r":
             print("removeolj")
         elif len(sys.argv) == 2 and sys.argv[1] == "-c":
             print("check")
         elif len(sys.argv) == 1:
-            print("menut irasd ki")
+            print("Command Line Todo application")
+            print("=============================")
+            print("Command line arguments:")
+            print(" -l   Lists all the tasks")
+            print(" -a   Adds a new task")
+            print(" -r   Removes an task")
+            print(" -c   Completes an task")
         
 
-    def todo_list_read(self, file_read):
-        self.todo_elements = self.file_read
-        self.todo_list = self.todo_elements.read()
-        print(self.todo_list)
+    def todo_list_read(self):
+        self.todo_elements = open("todo_list.txt", "r")
+        todo_list = self.todo_elements.read()
+        print(todo_list)
         self.todo_elements.close()
 
-    def add_todo(self, todo_input):
-        textfile = open("todo_list.txt", "w")
-        textfile.write("0 " + todo_input + "\n")
-        textfile.close()
 
-file_read = open("todo_list.txt", "r")
-file_write = open("todo_list.txt", "w")
+    def add_todo(self):
+        todo_input = input(str("Please write a new task: "))
+        self.textfile = open("todo_list.txt", "a")
+        self.textfile.write("0 " + todo_input + "\n")
+        self.textfile.close()
 
 
-todo = Todo_controller()
-todo.add_todo("walking dead")    
+
+
+
+todo = Todo_controller()   
 
 
 
